2f4e7f436066fc40a249acb1053a09d687970c62
Рамазан <you@example.com>
2024-01-17 16:22:48 +0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let form = document.querySelector('.form')
+let popup = ()=>{
+form.classList.toggle('active')
+}
+
+let log_form = document.querySelector('.log_form')
+let popin = ()=>{
+    log_form.classList.toggle('beactive')
+}
